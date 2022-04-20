f2582389db2f982fc9cfac4b94f0b5b76754bff9
arniecommits <arnab@cybernix.co.uk>
2022-04-20 20:11:31 +0100

    init

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "awsprops" {
+    type = "map"
+    default = {
+    region = "us-east-1"
+    vpc = "vpc-0e2998fe1122711f0"
+    ami = "ami-087c17d1fe0178315"
+    itype = "t2.micro"
+    subnet = "subnet-08962b20efe742225"
+    publicip = true
+    keyname = "aws-labs"
+    secgroupname = "SSHAccess"
+  }
+}
