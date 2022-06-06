96f87e21aa8fe5852439d7d867137021b83bd118
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2022-06-06 16:45:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-#define provider
 provider "aws" {
     region = lookup(var.awsprops, "region")
 }
@@ -16,6 +15,13 @@ resource "aws_security_group" "project-iac-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 80
+        protocol = "tcp"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+   
     egress {
         from_port = 0
         to_port = 0
@@ -25,34 +31,64 @@ resource "aws_security_group" "project-iac-sg" {
 
     lifecycle {
         create_before_destroy = true
-    }    
+    }
 
 
 }
 
 resource "aws_instance" "project-iac-ec2" {
-    
+
     ami = lookup(var.awsprops, "ami")
     instance_type = lookup(var.awsprops, "itype")
-    subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+    subnet_id = lookup(var.awsprops, "subnet")
     associate_public_ip_address = lookup(var.awsprops, "publicip")
     key_name = lookup(var.awsprops, "keyname")
-    
+    iam_instance_profile = lookup(var.awsprops, "role")
     vpc_security_group_ids = [
         aws_security_group.project-iac-sg.id
     ]
 
+    user_data = <<EOF
+#!/bin/bash
+sudo apt -y update && apt -y upgrade
+sudo apt -y install php libapache2-mod-php apache2 composer imagemagick
+sudo systemctl enable apache2
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+cd /var/www && sudo composer require aws/aws-sdk-php-resources
+sudo mkdir -p /var/www/data
+sudo rm -f /etc/ImageMagick-6/policy.xml
+sudo rm -rf /var/www/html/*
+sudo git clone https://github.com/dodgycoder/awss3pdfconverter.git /var/www/html/
+sudo chown -R www-data:www-data /var/www/
+sudo systemctl start apache2
+EOF
     root_block_device {
         delete_on_termination = true
-        iops = 150
         volume_size = 50
         volume_type = "gp2"
     }
 
     depends_on = [ aws_security_group.project-iac-sg ]
+    tags = {
+
+        Name = "PDFFiles-App-Node"
+        Environment = "Prod" 
+
+    }
 
 
+}
 
+resource "aws_s3_bucket" "s3bucket" {
+    bucket = lookup(var.awsprops,"bucketname") 
+    acl = lookup(var.awsprops,"bucketacl")
+    tags = {
+
+        Name = "PDFFiles"
+        Environment = "Prod" 
+
+    }   
 }
 
 output "ec2instance" {
