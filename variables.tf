7a9630077ac964700388f040a504804a29acfa6c
Arnab Roy <60926235+arniecommits@users.noreply.github.com>
2022-07-01 14:33:58 +0100

    Update variables.tf
    Updated to new cloud tenant details

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@ variable "awsprops" {
 
     type = "map"
     default = {
-    region = "us-east-1"
-    vpc = "vpc-0e2998fe1122711f0"
+    region = "eu-west-2"
+    vpc = "vpc-0a4d722728421a267"
     ami = "ami-087c17d1fe0178315"
     itype = "t2.micro"
-    subnet = "subnet-08962b20efe742225"
+    subnet = "subnet-07cef9974d911f488"
     publicip = true
-    keyname = "aws-labs"
-    secgroupname = "SSHAccess"
+    keyname = "arnab-key"
+    secgroupname = "Jenkins Access"
   }
 
 }
